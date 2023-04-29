f31cea8e3186588d110e40d29c5538cf2b34c612
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2023-04-29 11:56:06 +0200

    master->main

--- do_update.sh
+++ do_update.sh
@@ -2,7 +2,7 @@
 
 python3 grobtrack.py
 
-git add f3.poll  g4.poll  h4.poll  Na3.poll  Nh3.poll
-git add f3.png  g4.png  h4.png  Na3.png  Nh3.png
+git add f3.poll g4.poll h4.poll Na3.poll Nh3.poll
+git add f3.png g4.png h4.png Na3.png Nh3.png
 git diff --staged --quiet || git commit -m "Update results"
-git push origin master >& push.log
+git push origin main >& push.log
