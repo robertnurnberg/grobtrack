de99e953990a183471e4eb35ad81cdb3f6a284e7
Robert Nurnberg <robert.nurnberg@gmx.de>
2023-05-02 09:39:22 +0200

    changed wording

--- do_update.sh
+++ do_update.sh
@@ -4,5 +4,5 @@ python3 grobtrack.py
 
 git add f3.poll g4.poll h4.poll Na3.poll Nh3.poll
 git add f3.png g4.png h4.png Na3.png Nh3.png
-git diff --staged --quiet || git commit -m "Update results"
+git diff --staged --quiet || git commit -m "update poll data and plots"
 git push origin main >& push.log
