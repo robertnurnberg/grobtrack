b6ac98345cb28591640b53eb45d758e603cb69ee
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2023-05-03 17:11:27 +0200

    optimal graph data

--- grobtrack.py
+++ grobtrack.py
@@ -41,23 +41,31 @@ class polldata:
         by using different colors. PVs are truncated to pvLength to keep the
         number of unique PVs under control. Return number of colors needed.
         """
-        self.plotStart = plotStart # index to start plot from
-        self.pvLength = pvLength # length of truncated PVs
+        self.plotStart = plotStart  # index to start plot from
+        self.pvLength = pvLength  # length of truncated PVs
         self.colorId = []  # list of color ids to use for eval data points
         self.pvColor = {}  # dict that stores the color id used for each unique PV
         numberOfColors = 0  # number of different PVs => colors needed
-        for pv in self.pvs[self.plotStart:]:
+        for pv in self.pvs[self.plotStart :]:
             pvString = " ".join(m for m in pv[:pvLength])
             if pvString not in self.pvColor:
                 self.pvColor[pvString] = numberOfColors
                 numberOfColors += 1
             self.colorId.append(self.pvColor[pvString])
 
+    def create_optimal_graph_data(self, plotStart=0, pvLength=25):
+        # find optimal pvLength to still fit all unique PV strings on screen
+        while pvLength > 0:
+            self.create_graph_data(plotStart=plotStart, pvLength=pvLength)
+            if len(self.pvColor) <= 5 or len(self.pvColor) <= 10 and pvLength <= 12:
+                break
+            pvLength -= 1
+
     def create_graph(self, suffix="", cmapName="tab20b"):
-        # plot current graph data and save to file prefix.png 
-        date = self.date[self.plotStart:]
-        eval = self.eval[self.plotStart:]
-        depth = self.depth[self.plotStart:]
+        # plot current graph data and save to file prefix.png
+        date = self.date[self.plotStart :]
+        eval = self.eval[self.plotStart :]
+        depth = self.depth[self.plotStart :]
         fig, ax1 = plt.subplots()
         evalColor, dateColor, depthColor = "black", "black", "gray"
         if len(date) >= 400:
@@ -93,17 +101,23 @@ class polldata:
         )
         ax2.tick_params(axis="y", labelcolor=depthColor)
 
-        pvFontSize, moveWidth, maxLines = 6, 0.045, 5
+        pvFontSize, moveWidth, maxLines = 6, 0.049, 5
         top, stepsize, left, right = 1.12, 0.025, 0, 1
         if self.pvLength >= 13:
-            pvMaxDisplay = maxLines # maximal number of PVs we can show above plot
+            pvMaxDisplay = maxLines  # maximal number of PVs we can show above plot
         else:
             pvMaxDisplay = 2 * maxLines
-        textLength = self.pvLength if len(self.pvColor) <= maxLines else 2 * self.pvLength + 1
-        if textLength >= 25: # maximum to fit on screen with current font size
-            left = -0.15
-        elif textLength >= 22:
-            left = -0.1
+        textLength = (
+            self.pvLength if len(self.pvColor) <= maxLines else 2 * self.pvLength + 1
+        )
+        textLength = min(
+            textLength, 25
+        )  # maximum to fit on screen with current font size
+        if textLength > 20:
+            offset = 0.55 * moveWidth * (textLength - 20)
+            left -= offset
+            right += offset
+
         for i, pvString in enumerate(sorted(self.pvColor)[:pvMaxDisplay]):
             ax1.text(
                 left if i <= 4 else right - moveWidth * self.pvLength,
@@ -119,9 +133,11 @@ class polldata:
         plt.savefig(self.prefix + suffix + ".png", dpi=300)
 
 
-for move in [("g4", 3), ("h4", 8), ("Na3", 16), ("Nh3", 4), ("f3", 25)]:
-    data = polldata(move[0])
-    data.create_graph_data(plotStart=0, pvLength=move[1])
+for move in ["g4", "h4", "Na3", "Nh3", "f3"]:
+    data = polldata(move)
+    data.create_optimal_graph_data()
     data.create_graph()
-    data.create_graph_data(plotStart=-168, pvLength=move[1])
+    data.create_optimal_graph_data(plotStart=-168)
     data.create_graph(suffix="week")
+    data.create_optimal_graph_data(plotStart=-24)
+    data.create_graph(suffix="day")
