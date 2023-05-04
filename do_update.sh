1095043b220c037f3bcfb3f55bea60339f6dfb1f
Robert Nurnberg <robert.nurnberg@gmx.de>
2023-05-04 07:55:16 +0200

    fix missing images in daily update

--- do_update.sh
+++ do_update.sh
@@ -3,8 +3,8 @@
 python3 grobtrack.py
 
 git add f3.poll g4.poll h4.poll Na3.poll Nh3.poll
-git add f3.png g4.png h4.png Na3.png Nh3.png
-git add f3week.png g4week.png h4week.png Na3week.png Nh3week.png
-git add f3day.png g4day.png h4day.png Na3day.png Nh3day.png
+git add images/f3.png images/g4.png images/h4.png images/Na3.png images/Nh3.png
+git add images/f3week.png images/g4week.png images/h4week.png images/Na3week.png images/Nh3week.png
+git add images/f3day.png images/g4day.png images/h4day.png images/Na3day.png images/Nh3day.png
 git diff --staged --quiet || git commit -m "update poll data and plots"
 git push origin main >& push.log
