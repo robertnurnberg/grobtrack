924a1f735b9a142a05c7c5de59ba81e9936d1c6f
Robert Nurnberg <robert.nurnberg@gmx.de>
2023-10-08 21:07:04 +0200

    reformat shell scripts

--- do_update.sh
+++ do_update.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 if [[ ! -e images ]]; then
-  mkdir images
+    mkdir images
 fi
 
 python3 grobtrack.py
 
-for m in g4 h4 Na3 Nh3 f3
-do
-  git add "$m".poll
-  git add images/"$m".png images/"$m"week.png images/"$m"day.png 
+for m in g4 h4 Na3 Nh3 f3; do
+    git add "$m".poll
+    git add images/"$m".png images/"$m"week.png images/"$m"day.png
 done
 
 git diff --staged --quiet || git commit -m "update poll data and plots"
-git push origin main >& push.log
+git push origin main >&push.log
 
-./do_sfwdl.sh >& sfwdl.log
+./do_sfwdl.sh >&sfwdl.log
